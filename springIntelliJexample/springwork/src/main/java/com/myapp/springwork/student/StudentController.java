2bb221a2ca0c740701b44efacac3f6f49be085e0
Pia <piatorain@yahoo.com>
2022-10-06 14:14:53 -0700

    local db setup

--- springIntelliJexample/springwork/src/main/java/com/myapp/springwork/student/StudentController.java
+++ springIntelliJexample/springwork/src/main/java/com/myapp/springwork/student/StudentController.java
@@ -1,6 +1,7 @@
 package com.myapp.springwork.student;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,14 @@ public class StudentController {
     private final StudentService studentService;
 
     // P4d constructor
+    // P5 will not work because there is not an instance of student service
+        // need to studentService field to be injected into controller
+        // use dependency injection - @Autowired
+            // instantiates and injects studentService into
+        // must make sure the StudentService class is a Bean in order for
+            // Spring to know that it should be autowired
+            // use @Service, same as @Component
+    @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
--- springIntelliJexample/springwork/src/main/java/com/myapp/springwork/student/StudentService.java
+++ springIntelliJexample/springwork/src/main/java/com/myapp/springwork/student/StudentService.java
@@ -1,10 +1,15 @@
 package com.myapp.springwork.student;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
-//Part 4 - create student service
+// P5 - applied @Service annotation to StudentServices, same as @Component
+    // Spring will scan and identify it as a Bean and autowire it where indicated
+@Service
 public class StudentService {
 
     // removed method from StudentController
